6f5954bbe5cce0ef143b1dde5d0919c89357e139
Truong Do Quoc Anh <anh.tdq2416859@sis.hust.edu.vn>
2026-04-17 15:54:01 +0700

    Refactor

---
+++ Lab03/OtherProjects/src:hust:soict:hedspi/Lab01/make_triangle.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+public class make_triangle {
+	public static void main(String args[]) {
+		System.out.println("Nhap mot so duong bat ki");
+		int n=new Scanner(System.in).nextInt();
+		for(int i=0;i<n;i++)
+		{
+			for(int j=n-i;j>0;j--) System.out.printf(" ");
+			for(int k=1;k<=1+2*i;k++) System.out.printf("*");
+			System.out.printf("\n");
+		}
+		
+	}
+}
